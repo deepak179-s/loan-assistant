daf8e757c645ff651641c997c7b1061072f86903
deepak179-s <deepak17943@gmail.com>
2026-03-26 22:09:34 +0530

    Fix Express 5 catch-all route syntax error

--- server.js
+++ server.js
@@ -148,7 +148,7 @@ app.post('/api/chat', async (req, res) => {
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Wildcard route to serve index.html for SPA routing
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
